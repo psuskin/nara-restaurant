505101a54e10b6dcaf5dec89c5171aadf75d1423
psuskin <philsuskin@gmail.com>
2025-04-17 00:11:08 +0200

    Fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import RestaurantButton from "./Restaurent/RestaurantButton";
+// import RestaurantButton from "./Restaurent/RestaurantButton";
 
 interface FooterProps {
   dict: {
